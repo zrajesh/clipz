36a52402e7f7519c3195aa3f4bb341b9b4f0f555
rajeshprofile <hirajesh.official@gmail.com>
2022-08-04 14:44:53 +0530

    added modal auth

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,12 +7,16 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
-  constructor(public modal: ModalService) { }
+  constructor(public modal: ModalService, public el: ElementRef) { }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
   closeModal() {
-    this.modal.toggleModal();
+    this.modal.toggleModal(this.modalID);
   }
 }
+
+
