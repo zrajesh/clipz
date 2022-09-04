6a2450e421fd225ebff3dd6c7d595f73178b5489
rajeshprofile <hirajesh.official@gmail.com>
2022-09-04 13:33:55 +0530

    added login logic

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  credentials = {
+    email: '',
+    password: ''
+  }
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  login() {
+    console.log(this.credentials);
+  }
 
 }
